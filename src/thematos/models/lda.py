eb0d8a2f106981ee473c3ce9cf589aaddf955979
Young Joon Lee <entelecheia@hotmail.com>
2023-08-10 21:17:09 +0900

    refactor(lda.py): model arguments update and training process adjustments

--- src/thematos/models/lda.py
+++ src/thematos/models/lda.py
@@ -2,55 +2,37 @@ from typing import Optional
 
 import tomotopy as tp
 from hyfi import HyFI
-from hyfi.composer import BaseModel
+from tqdm.auto import tqdm
 
 from .base import TopicModel
-from .types import IDF
+from .config import LdaConfig
 
 logger = HyFI.getLogger(__name__)
 
 
-class LdaConfig(BaseModel):
-    _config_group_ = "/model/config"
-    _config_name_ = "lda"
-
-    tw: str = IDF
-    min_cf: int = 5
-    min_df: int = 0
-    rm_top: int = 0
-    k: Optional[int] = None
-    alpha: float = 0.1
-    eta: float = 0.01
-
-
 class LdaModel(TopicModel):
     _config_name_ = "lda"
+    _auto_populate_ = False
 
     model_type: str = "LDA"
-    model_config: LdaConfig = LdaConfig()
+    model_args: LdaConfig = LdaConfig()
 
     # internal attributes
     _model_: Optional[tp.LDAModel] = None
 
     @property
-    def model_id(self) -> str:
-        model_type = self.model_type.upper()
-        margs = [model_type, self.batch_name, f"k({self.model_config.k})"]
-        return "_".join(margs)
-
-    @property
     def model(self) -> tp.LDAModel:
         if self._model_ is None:
             self._model_ = tp.LDAModel(
-                corpus=self.corpus,
-                seed=self.seed,
-                **self.model_config.model_dump(),
+                corpus=self.tp_corpus,
+                seed=self.train_args.seed,
+                **self.model_args.model_dump(),
             )
         return self._model_
 
     def _train(self, model: tp.LDAModel) -> None:
-        train_cfg = self.train_config
-        model.burn_in = train_cfg.burn_in
+        train_args = self.train_args
+        model.burn_in = train_args.burn_in
         model.train(0)
         logger.info("Number of docs: %s", len(model.docs))
         logger.info("Vocab size: %s", model.num_vocabs)
@@ -58,13 +40,13 @@ class LdaModel(TopicModel):
         logger.info("Removed top words: %s", model.removed_top_words)
         logger.info(
             "Training model by iterating over the corpus %s times, %s iterations at a time",
-            self.iterations,
-            self.interval,
+            train_args.iterations,
+            train_args.interval,
         )
 
         ll_per_words = []
-        for i in range(0, train_cfg.iterations, train_cfg.interval):
-            model.train(train_cfg.interval)
+        for i in tqdm(range(0, train_args.iterations, train_args.interval)):
+            model.train(train_args.interval)
             logger.info("Iteration: %s\tLog-likelihood: %s", i, model.ll_per_word)
             ll_per_words.append((i, model.ll_per_word))
         self._ll_per_words_ = ll_per_words
