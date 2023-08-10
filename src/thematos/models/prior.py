3d330c2a520b4239c20a5ac35b65e863e89b26db
Young Joon Lee <entelecheia@hotmail.com>
2023-08-10 18:12:27 +0900

    refactor(prior): change 'words' to 'priors' and improve load and save methods functionality

--- src/thematos/models/prior.py
+++ src/thematos/models/prior.py
@@ -1,88 +1,91 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union
 
 from hyfi import HyFI
 from hyfi.composer import BaseModel
 
 logger = HyFI.getLogger(__name__)
 
+PriorType = Dict[int, List[str]]
+
 
 class WordPrior(BaseModel):
     _config_group_: str = "/words"
-    _config_name_: str = "wordprior"
+    _config_name_: str = "_wordprior_"
 
     name: str = "wordprior"
-    lowercase: bool = False
-    words_list: Optional[Dict[str, float]] = None
-    words_path: Optional[str] = None
+    lowercase: bool = True
+    prior_data: Optional[PriorType] = None
+    data_file: Optional[str] = None
     verbose: bool = False
 
-    _loaded_: bool = False
-    _words_: Dict[str, float] = {}
+    _priors_: PriorType = {}
 
     @property
-    def words(self) -> List[str]:
-        if self._loaded_:
-            return self._words_
-        self.load()
-        return self._words_
+    def priors(self) -> List[str]:
+        if not self._priors_:
+            self.load()
+        return self._priors_
+
+    def add(self, id_: int, prior: Union[str, List[str]]):
+        """Add a prior to the word prior"""
+        if isinstance(prior, str):
+            prior = [prior]
+        prior = [w.lower() for w in prior] if self.lowercase else prior
+        self._priors_[id_] = list(set(self._priors_.get(id_, []) + prior))
 
     def load(self):
         """Load words from file or function"""
-        if self.words_path:
-            self._words_ = HyFI.to_dict(HyFI.load(self.words_path))
+        if self.data_file:
+            self._priors_ = HyFI.to_dict(HyFI.load(self.data_file))
             if self.verbose:
                 logger.info(
                     "Loaded %d words from %s",
-                    len(self._words_),
-                    self.words_path,
+                    len(self._priors_),
+                    self.data_file,
                 )
         else:
-            self._words_ = {}
+            self._priors_ = {}
 
-        if self.words_list:
-            for word, prior in self.words_list.items():
-                self._words_[word] = prior
+        if self.prior_data:
+            for id_, prior in self.prior_data.items():
+                self._priors_[id_] = prior
 
-        if self.lowercase:
-            self._words_ = {word.lower(): prior for word, prior in self._words_.items()}
+        self._priors_ = {
+            int(id_): list({w.lower() if self.lowercase else w for w in prior})
+            for id_, prior in self._priors_.items()
+        }
         if self.verbose:
-            logger.info("Loaded %d words", len(self._words_))
-        self._loaded_ = True
+            logger.info("Loaded %d priors", len(self._priors_))
 
     def save(self):
         """Save words to file"""
-        if not self._loaded_:
+        if not self.priors:
+            logger.warning("No priors to save")
             return
-        HyFI.save(self._words_, self.words_path)
+        HyFI.save(self.priors, self.data_file)
         if self.verbose:
-            logger.info("Saved %d words to %s", len(self._words_), self.words_path)
+            logger.info("Saved %d words to %s", len(self.priors), self.data_file)
 
     def items(self):
-        return self.words.items()
+        return self.priors.items()
 
     def __iter__(self):
-        return iter(self.words)
+        return iter(self.priors)
 
     def __len__(self):
-        return len(self.words)
-
-    def __contains__(self, word):
-        return self.words.__contains__(word)
-
-    def __getitem__(self, word):
-        return self.words.__getitem__(word)
+        return len(self.priors)
 
     def __repr__(self):
-        return f"<WordPrior {len(self.words)} words>"
+        return f"<WordPrior {len(self.priors)} priors>"
 
     def __str__(self):
-        return f"<WordPrior {len(self.words)} words>"
+        return f"<WordPrior {len(self.priors)} priors>"
 
     def __bool__(self):
-        return bool(self.words)
+        return bool(self.priors)
 
     def __eq__(self, other: "WordPrior"):
-        return self.words == other.words
+        return self.priors == other.priors
 
     def __ne__(self, other: "WordPrior"):
-        return self.words != other.words
+        return self.priors != other.priors
