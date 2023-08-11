6f128bdc011249add52dda2e486e6e8690651922
Young Joon Lee <entelecheia@hotmail.com>
2023-08-11 12:00:55 +0900

    feat(lda): add model loading functionality

--- src/thematos/models/lda.py
+++ src/thematos/models/lda.py
@@ -52,3 +52,12 @@ class LdaModel(TopicModel):
         if self.verbose:
             model.summary()
         self._model_ = model
+
+    def _load_model(self):
+        model_path = self.model_file
+        if HyFI.is_file(model_path):
+            self.model = tp.LDAModel.load(model_path)
+            logger.info("Model loaded from %s", model_path)
+        else:
+            self._model_ = None
+            logger.warning("Model file %s does not exist", model_path)
