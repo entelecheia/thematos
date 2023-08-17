b30ae141ea6788c94709de439ec6699611b63c50
Young Joon Lee <entelecheia@hotmail.com>
2023-08-18 04:54:02 +0900

    chore(thematos/models): add TODO comments for analysis classes

--- src/thematos/models/__init__.py
+++ src/thematos/models/__init__.py
@@ -3,6 +3,9 @@ from .config import LdaConfig, TrainConfig, TrainSummaryConfig, WordcloudConfig
 from .lda import LdaModel
 from .prior import WordPrior
 
+# TODO: Add analysis classes
+# from .analysis import TopicAnalysis, TopicAnalysisConfig
+
 __all__ = [
     "LdaModel",
     "LdaConfig",
